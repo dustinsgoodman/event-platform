b8dd55d9f270d978104d0be605ebeac3c67d8fa3
Dustin Goodman <dustin@thisdot.co>
2023-09-09 23:21:52 -0500

    add table component

---
+++ web/src/components/Table/Table.mock.tsx
@@ -0,0 +1,29 @@
+import { createColumnHelper } from '@tanstack/react-table'
+
+export type TableData = {
+  id: number;
+  name: string;
+};
+
+const columnHelper = createColumnHelper<TableData>();
+export const standard = () => ({
+  data: [
+    { id: 1, name: 'A' },
+    { id: 2, name: 'B' },
+    { id: 3, name: 'C' },
+  ],
+  columns: [
+    columnHelper.accessor('id', {
+      header: () => 'ID',
+      cell: info => info.getValue(),
+    }),
+    columnHelper.accessor('name', {
+      header: () => 'Name',
+      cell: info => info.getValue(),
+    }),
+    columnHelper.display({
+      id: 'actions',
+      cell: info => (<button>press me</button>)
+    }),
+  ],
+});
