219dea75007c2c819d22b63eaf1402179293788d
Dustin Goodman <dustin@thisdot.co>
2023-09-09 23:22:10 -0500

    update Events to use new Table

--- web/src/components/Event/Events/Events.tsx
+++ web/src/components/Event/Events/Events.tsx
@@ -4,9 +4,12 @@ import { Link, routes } from '@redwoodjs/router';
 import { useMutation } from '@redwoodjs/web';
 import { toast } from '@redwoodjs/web/toast';
 
+import { createColumnHelper } from '@tanstack/react-table'
+
 import { QUERY } from 'src/components/Event/EventsCell';
 import Pagination from 'src/components/Pagination/Pagination';
 import { timeTag } from 'src/lib/formatters';
+import Table from 'src/components/Table/Table';
 
 const DELETE_EVENT_MUTATION = gql`
   mutation DeleteEventMutation($id: String!) {
@@ -37,69 +40,72 @@ const EventsList = ({ events }: FindEvents) => {
     }
   };
 
+  const columnHelper = createColumnHelper<FindEvents['events']['nodes'][0]>();
+  const columns = [
+    columnHelper.accessor('name', {
+      header: () => 'Name',
+      cell: info => (
+        <Link
+          to={routes.event({ id: info.row.original.id })}
+          className="text-blue-500 hover:text-blue-800"
+        >
+          {info.getValue()}
+        </Link>
+      )
+    }),
+    columnHelper.accessor('startAt', {
+      header: () => 'Event Starts At',
+      cell: info => timeTag(info.getValue()),
+    }),
+    columnHelper.accessor('endAt', {
+      header: () => 'Event Ends At',
+      cell: info => timeTag(info.getValue()),
+    }),
+    columnHelper.accessor('registrationStartAt', {
+      header: () => 'Registration Starts At',
+      cell: info => timeTag(info.getValue()),
+    }),
+    columnHelper.accessor('registrationEndAt', {
+      header: () => 'Registration Ends At',
+      cell: info => timeTag(info.getValue()),
+    }),
+    columnHelper.display({
+      id: 'actions',
+      cell: info => (
+        <nav className="flex justify-end items-center h-4 pr-1">
+          <Link
+            to={routes.event({ id: info.row.original.id })}
+            title={`Show event ${info.row.original.id} detail`}
+            className="bg-transparent hover:bg-gray-500 hover:text-white text-xs rounded-sm py-1 px-2"
+          >
+            Show
+          </Link>
+          <Link
+            to={routes.editEvent({ id: info.row.original.id })}
+            title={`Edit event ${info.row.original.id}`}
+            className="text-blue-500 hover:bg-blue-500 hover:text-white bg-transparent text-xs rounded-sm py-1 px-2 rw-button-blue"
+          >
+            Edit
+          </Link>
+          <button
+            type="button"
+            title={`{Delete event ${info.row.original.id}`}
+            className="text-red-600 bg-transparent hover:bg-red-600 hover:text-white text-xs rounded-sm py-1 px-2 rw-button-red"
+            onClick={() => onDeleteClick(info.row.original.id, info.row.original.name)}
+          >
+            Delete
+          </button>
+        </nav>
+      )
+    }),
+  ]
+
   return (
     <>
-      <div className="rounded-lg overflow-hidden w-full border border-gray-200 overflow-x-auto mb-4">
-        <table className="w-full text-sm">
-          <thead>
-            <tr className="bg-gray-200 text-gray-600 text-left">
-              <th className="p-3">Name</th>
-              <th className="p-3">Event Starts At</th>
-              <th className="p-3">Event Ends At</th>
-              <th className="p-3">Registration Starts At</th>
-              <th className="p-3">Registration Ends At</th>
-              <th className="p-3">&nbsp;</th>
-            </tr>
-          </thead>
-          <tbody>
-            {events.nodes.map((event) => (
-              <tr
-                key={event.id}
-                className="odd:bg-gray-50 border-t border-gray-200"
-              >
-                <td className="p-3">
-                  <Link
-                    to={routes.event({ id: event.id })}
-                    className="text-blue-500 hover:text-blue-800"
-                  >
-                    {event.name}
-                  </Link>
-                </td>
-                <td className="p-3">{timeTag(event.startAt)}</td>
-                <td className="p-3">{timeTag(event.endAt)}</td>
-                <td className="p-3">{timeTag(event.registrationStartAt)}</td>
-                <td className="p-3">{timeTag(event.registrationEndAt)}</td>
-                <td className="p-3">
-                  <nav className="flex justify-end items-center h-4 pr-1">
-                    <Link
-                      to={routes.event({ id: event.id })}
-                      title={'Show event ' + event.id + ' detail'}
-                      className="bg-transparent hover:bg-gray-500 hover:text-white text-xs rounded-sm py-1 px-2"
-                    >
-                      Show
-                    </Link>
-                    <Link
-                      to={routes.editEvent({ id: event.id })}
-                      title={'Edit event ' + event.id}
-                      className="text-blue-500 hover:bg-blue-500 hover:text-white bg-transparent text-xs rounded-sm py-1 px-2 rw-button-blue"
-                    >
-                      Edit
-                    </Link>
-                    <button
-                      type="button"
-                      title={'Delete event ' + event.id}
-                      className="text-red-600 bg-transparent hover:bg-red-600 hover:text-white text-xs rounded-sm py-1 px-2 rw-button-red"
-                      onClick={() => onDeleteClick(event.id, event.name)}
-                    >
-                      Delete
-                    </button>
-                  </nav>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <Table<FindEvents['events']['nodes'][0]>
+        columns={columns}
+        data={events.nodes}
+      />
       <Pagination
         route={routes.events}
         totalPages={events.pagination.totalPages}
