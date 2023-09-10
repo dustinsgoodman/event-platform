416ab7d08d36ff982b6222210b4088bc68f1b477
Dustin Goodman <dustin@thisdot.co>
2023-09-09 23:50:01 -0500

    conditionally render Pagination buttons

--- web/src/components/Pagination/Pagination.tsx
+++ web/src/components/Pagination/Pagination.tsx
@@ -13,25 +13,31 @@ const Pagination: FC<PaginationProps> = ({
   totalPages,
   currentPage,
 }) => {
+  const showButtons = totalPages !== 1;
+
   return (
     <div className="flex items-center justify-center gap-4">
-      <button
-        onClick={() => navigate(route({ page: currentPage - 1}))}
-        className="flex justify-center py-1 px-4 mx-1 border-0 rounded bg-blue-500 text-white hover:bg-blue-700 text-xs font-semibold uppercase tracking-wide leading-loose no-underline cursor-pointer transition duration-100 disabled:opacity-50"
-        disabled={currentPage <= 1}
-      >
-        Prev
-      </button>
+      {showButtons && (
+        <button
+          onClick={() => navigate(route({ page: currentPage - 1}))}
+          className="flex justify-center py-1 px-4 mx-1 border-0 rounded bg-blue-500 text-white hover:bg-blue-700 text-xs font-semibold uppercase tracking-wide leading-loose no-underline cursor-pointer transition duration-100 disabled:opacity-50"
+          disabled={currentPage <= 1}
+        >
+          Prev
+        </button>
+      )}
       <div className="text-sm">
         Page {currentPage} of {totalPages}
       </div>
-      <button
-        onClick={() => navigate(route({ page: currentPage + 1}))}
-        className="flex justify-center py-1 px-4 mx-1 border-0 rounded bg-blue-500 text-white hover:bg-blue-700 text-xs font-semibold uppercase tracking-wide leading-loose no-underline cursor-pointer transition duration-100 disabled:opacity-50"
-        disabled={currentPage >= totalPages}
-      >
-        Next
-      </button>
+      {showButtons && (
+        <button
+          onClick={() => navigate(route({ page: currentPage + 1}))}
+          className="flex justify-center py-1 px-4 mx-1 border-0 rounded bg-blue-500 text-white hover:bg-blue-700 text-xs font-semibold uppercase tracking-wide leading-loose no-underline cursor-pointer transition duration-100 disabled:opacity-50"
+          disabled={currentPage >= totalPages}
+        >
+          Next
+        </button>
+      )}
     </div>
   );
 };
