48d0690dc61b3e6faca76653c4bf96e849d3f420
Dustin Goodman <dustin.s.goodman@gmail.com>
2023-10-05 15:12:54 -0500

    scaffold speakers and registrants pages

---
+++ web/src/pages/EventRegistrant/EventRegistrantsPage/EventRegistrantsPage.test.tsx
@@ -0,0 +1,14 @@
+import { render } from "@redwoodjs/testing/web";
+
+import EventRegistrantsPage from "./EventRegistrantsPage";
+
+//   Improve this test with help from the Redwood Testing Doc:
+//   https://redwoodjs.com/docs/testing#testing-pages-layouts
+
+describe("EventRegistrantsPage", () => {
+  it("renders successfully", () => {
+    expect(() => {
+      render(<EventRegistrantsPage />);
+    }).not.toThrow();
+  });
+});
