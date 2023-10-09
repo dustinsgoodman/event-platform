0da922f3bc6b3421f78e4a0436a06c3ab9803bec
Dustin Goodman <dustin.s.goodman@gmail.com>
2023-10-08 21:00:27 -0500

    fix i18n testing setup to suppress warnings

--- web/src/pages/Event/EventPage/EventPage.test.tsx
+++ web/src/pages/Event/EventPage/EventPage.test.tsx
@@ -5,7 +5,7 @@ import EventPage from './EventPage';
 describe('EventPage', () => {
   it('renders successfully', () => {
     expect(() => {
-      render(<EventPage id="97d64e90-2c53-41cf-a6a5-af399f75ecc5" />);
+      render(<EventPage eventId="97d64e90-2c53-41cf-a6a5-af399f75ecc5" />);
     }).not.toThrow();
   });
 });
